4c907f22ac1dd6fd572298f3867337c7d961d65f
seansg <seansgarwood@gmail.com>
2023-01-10 17:47:36 -0500

    Rewrite problem/plan, test eventListener

--- script.js
+++ script.js
@@ -1,41 +1,17 @@
 /* problem
-create a 16 x 16 grid of square divs, using only javascript dom methods.
+set up a hover event listener so grid divs change color when mouse passes over
+them.
+
+This should leave a trail through the grid, like a pen would.
 
 plan
-I will define a variable that represents a div. Then I will use DOM methods to
-create additional instances of this div (256 to be exact). I will probably need
-to use a for-loop to do this.
+I need ot set up event listener for when a mouse enters a div, and/or when it
+leaves a div.
 
-Then, I will need to make sure the style of the grid is correct. 
-
--16 on each row
--16 rows
--square in shape
-
-I can define a class in my external style.css, then add that class to the divs
-that were created with the dom methods.
-
-might be best to create sixteen container divs that are flexboxes, then create
-16 divs in each one of those.
-
-no user interface yet.
-no inputs
-desired output is 16x16 grid.
-create instance of a div dom object. call it a container.
-use dom methods to create 16 of these, then 16 squares inside those.
-
+This can be done when the mouse hovers over a cell, or when it leaves a div (or
+both?)
+ 
 pseudocode
-
-create div reference
-create sixteen instances of div, make them containers that are the length of 16
-square sides, and the height of one.
-  for loop: counter = 0, counter < 16, counter++
-    append div to body
-  for loop: counter = 0, counter < 16, counter++
-    append div to container
-link appropriate classes to these newly-created divs (i.e. container, square)
-
-Pick the simplest one above: create div reference.
 
 */
 const container = document.getElementById('container');
@@ -43,5 +19,6 @@ const div = document.createElement('div');
 div.className = 'item';
 
 for(let i = 0; i < 256; i++){
+  div.textContent = `${i+1}`;
   container.appendChild(div.cloneNode(true));
 }
