84e22e6e8e2a39e03673ebb9d284df5080c9129e
seansg <seansgarwood@gmail.com>
2023-01-11 21:34:28 -0500

    Define colorInDivs

--- script.js
+++ script.js
@@ -1,14 +1,9 @@
 /* 
 #problem
 
-Add a button to the top of the screen that, when pressed, prompts the user for
-the number of squares per side for the grid.
-
 Once the prompt is complete, the old grid should be removed, and replaced with
 the new grid in the same total space as before. (Currently, the grid is not
 limited in size.)
-
-a good starting limit for the absolute size of the grid is 960px.
 
 #plan
 
@@ -16,8 +11,7 @@ Store the value from the prompt into a gridSize variable.
 check if gridSize is an int between 1 and 100.
   if not, start over
 clear old grid items
-pass gridSize to CSS sheet
-
+define new grid size using DOM method on the .container object
 
 
 #pseudocode
@@ -35,17 +29,21 @@ function setGridSize() {
   for(let i = 0; i < gridSize ** 2; i++){
     container.appendChild(div.cloneNode(true));
   }
+  colorInDivs();
+}
+
+function removeGridItems() {
+  let gridItems = Array.from(document.getElementsByClassName('item'));
+  gridItems.forEach(gridItem => gridItem.remove());
+}
+
+function colorInDivs() {
   const items = Array.from(document.getElementsByClassName('item'));
   items.forEach(item => {
     item.addEventListener('mouseover', e => {
       item.setAttribute('style', 'background-color: yellow;')
     });
   });
-}
-
-function removeGridItems() {
-  let gridItems = Array.from(document.getElementsByClassName('item'));
-  gridItems.forEach(gridItem => gridItem.remove());
 }
 
 const button = document.querySelector('button');
