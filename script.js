f96b7f10e0004af868c40336ffce700fc4b1ecb3
seansg <seansgarwood@gmail.com>
2023-01-13 13:36:35 -0500

    Comment out grayscale functions/buttons

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 /*
 problem
-Whenever the cursor passes through a pixel div, change its background-color
-attribute to be a random rgb value.
+Whenever the cursor passes through the first pixel, set its opacity to 10%
+(0.1). The next pixel should have an opacity of 0.2, then 0.3 ... 1.0, then 0.1.
 
 plan
 rewrite the colorInDivs function so that it returns a random RGB value rather
@@ -16,12 +16,33 @@ function removeGridItems() {
   gridItems.forEach(gridItem => gridItem.remove());
 }
 
-function colorInDivs() {
+function colorInDivsBlack() {
   const items = Array.from(document.getElementsByClassName('item'));
   items.forEach(item => {
     item.addEventListener('mouseover', e => {
-      item.setAttribute('style', `background-color: rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)});`)
+      item.setAttribute('style', 'background-color: black;');
     });
+  });
+}
+
+function colorInDivsRandom() {
+  const items = Array.from(document.getElementsByClassName('item'));
+  items.forEach(item => {
+    item.addEventListener('mouseover', e => {
+      item.setAttribute('style', `background-color: rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)});`);
+    });
+  });
+}
+
+function colorInDivsGrayscale() {
+  const items = Array.from(document.getElementsByClassName('item'));
+  items.forEach(item => {
+    for (let i; i <= 1; i += 0.1) {
+      opacity = i;
+      item.addEventListener('mouseover', e => {
+        item.setAttribute('style', `background-color: black; opacity: ${opacity};`);
+      });
+    };
   });
 }
 
@@ -34,18 +55,44 @@ function buildGrid(gridSize) {
   }
 }
 
-function setGridSize() {
+function setGridSizeBlack() {
   let gridSize = prompt('Set the grid size to an integer between 1 and 100');
   if (gridSize % 1 !== 0 || gridSize < 1 || gridSize > 100 ) {
-    return setGridSize();
+    return setGridSizeBlack();
   }
   removeGridItems();
   buildGrid(gridSize);
-  colorInDivs();
+  colorInDivsBlack();
 }
 
-const button = document.querySelector('button');
-button.addEventListener('click', setGridSize);
+function setGridSizeRandom() {
+  let gridSize = prompt('Set the grid size to an integer between 1 and 100');
+  if (gridSize % 1 !== 0 || gridSize < 1 || gridSize > 100 ) {
+    return setGridSizeRandom();
+  }
+  removeGridItems();
+  buildGrid(gridSize);
+  colorInDivsRandom();
+}
+
+function setGridSizeGrayscale() {
+  let gridSize = prompt('Set the grid size to an integer between 1 and 100');
+  if (gridSize % 1 !== 0 || gridSize < 1 || gridSize > 100 ) {
+    return setGridSizeGrayscale();
+  }
+  removeGridItems();
+  buildGrid(gridSize);
+  colorInDivsGrayscale();
+}
+
+const blackButton = document.getElementById('black');
+blackButton.addEventListener('click', setGridSizeBlack);
+
+const randomButton = document.getElementById('random');
+randomButton.addEventListener('click', setGridSizeRandom);
   
+const grayscaleButton = document.getElementById('grayscale');
+grayscaleButton.addEventListener('click', setGridSizeGrayscale);
+
 const div = document.createElement('div');
 div.className = 'item';
